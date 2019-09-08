a4d04812ca271e7ee860a09084e52468ce804e7c
Lucas Figueiredo <lukas.liberato14@gmail.com>
2019-09-08 11:57:14 -0300

    Passo 4 - Iniciando nossos teste - Integração e unitário

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ comandos aula 03
 yarn sequelize init
 yarn add dotenv - configurar as variaveis ambiente
 yarn sequelize migration:create --name=create-users // criar migration com a lib seq..
+
+comandos aula 04
+yarn add supertest -D 
--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,19 @@
 const routes = require('express').Router();
 
-const { User } = require('./app/models/User');
+// const { User } = require('./app/models/User');
 
-routes.get('/', async (req, res) => {
-    const user = await User.create({
-        user: "Lucas",
-        email: "lukas@gmail.com",
-        password_hash: "123123"
-    });
+// routes.get('/', async (req, res) => {
+//     const user = await User.create({
+//         user: "Lucas",
+//         email: "lukas@gmail.com",
+//         password_hash: "123123"
+//     });
 
-    return res.json(user);
-});
+//     return res.json(user);
+// });
+
+const SessionController = require("./app/controllers/SessionController");
+
+routes.post("/sessions", SessionController.store);
 
 module.exports = routes;
